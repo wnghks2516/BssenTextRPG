5db44419ec413afe1dc5a0677a69de489754f2c6
wnghks2516 <wnghks2516@naver.com>
2026-02-22 16:17:22 +0900

    Set Game Title

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace BssenTextRPG
+﻿using TextRPG.Utils;
+
+namespace BssenTextRPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Todo 타이틀 표시 
+            ConsoleUI.ShowTitle();
+            //Todo 저장된 게임 존재 여부 확인
+
+            //Todo 게임 로드 및 새 게임 시작
+
+
+
+
         }
     }
 }
