9425439dd43b3050f4a914712c632a3c36881a2a
wnghks2516 <wnghks2516@naver.com>
2026-02-23 13:44:34 +0900

    GameManager : battleSystem 출력 테스트 추가

--- Data/GameManager.cs
+++ Data/GameManager.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using TextRPG.Utils;
 using TextRPG.Models;
+using TextRPG.Systems;
 
 namespace TextRPG.Data;
 
@@ -121,8 +122,13 @@ public class GameManager
         Player = new Player(name, jobType, gold: 1000);
         Console.WriteLine($"'{Player.Name}'님이 '{Player.Job}' 직업으로 생성되었습니다. 모험을 시작하겠습니다!\n");
         Player.DisplayInfo();
-        //테스트 코드
-        // Console.WriteLine($"[테스트] 플레이어 정보 : 이름={Player.Name}, 직업={Player.Job}, HP={Player.CurrentHP}/{Player.MaxHP}, MP={Player.CurrentMP}/{Player.MaxMP}, 공격력={Player.AttackPower}, 방어력={Player.Defense}, 레벨={Player.Level}, 골드={Player.Gold}");
+
+        Enemy enemy = Enemy.CreateEnemy(Player.Level);
+        enemy.DisplayInfo();
+
+        BattleSystem battleSystem = new BattleSystem();
+        battleSystem.StartBattle(Player, enemy);
+
         ConsoleUI.PressAnyKey();
     }
     #endregion
